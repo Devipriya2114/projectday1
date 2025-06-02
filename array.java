9c789e9002fdfb4c47fc7bdbaf87eee290a53647
Devipriya2114 <ddpriya2114@gmail.com>
2025-06-02 12:57:05 +0530

    Update array.java

--- array.java
+++ array.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class array {
    public static void main(String[] args) {
-	   
+	   int a =10;
 	  String[] java = new String[4];
 	  java[0]="j";
 	  java[1]="a";

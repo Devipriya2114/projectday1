1607a1e946cbd067e7555a08319dcbd905f6a7c2
Devipriya2114 <ddpriya2114@gmail.com>
2025-06-02 12:45:47 +0530

    array added

---
+++ array.java
@@ -0,0 +1,19 @@
+package hello1;
+
+import java.util.Arrays;
+
+public class array {
+   public static void main(String[] args) {
+	   
+	  String[] java = new String[4];
+	  java[0]="j";
+	  java[1]="a";
+	  java[2]="v";
+	  java[3]="a";
+	  System.out.println(Arrays.toString(java));
+   }
+}
+	//  String[] java1 = 
+	  
+
+
